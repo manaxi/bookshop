daf7477dca5143dc7bbc7acb2e92b67cc0a94252
manaxi <manaxi17@gmail.com>
2021-02-09 23:50:34 +0200

    users auth and roles with permissions

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PagesController;
+use App\Http\Controllers\UsersController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +14,12 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Auth::routes();
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [PagesController::class, 'index']);
+
+Route::prefix('/settings')->name('settings.')->group(function () {
+    Route::get('/', [UsersController::class, 'profile'])->name('profile');
+    Route::post('settings', [UsersController::class, 'updateProfile'])->name('updateProfile');
+    Route::post('settings/password', [UsersController::class, 'updatePassword'])->name('changePassword');
 });
