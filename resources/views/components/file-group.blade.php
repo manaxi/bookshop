1c8f13559463603bc17df993fd4251a38a1cd870
manaxi <manaxi17@gmail.com>
2021-02-17 00:34:29 +0200

    removed datatable from user

--- resources/views/components/file-group.blade.php
+++ resources/views/components/file-group.blade.php
@@ -2,5 +2,5 @@
     <label for="{{ $fieldName }}" class="col-form-label">{{ __($fieldLabel) }}</label>
     <input id="{{ $fieldName }}" type="{{ $fieldType }}" class=" @error($fieldName) is-invalid @enderror"
            name="{{ $fieldName }}"
-           value="{{ $fieldValue ?? old($fieldName) }}"  autocomplete="{{ $fieldName }}" autofocus>
+           value="{{ $fieldValue ?? old($fieldName) }}"  autocomplete="{{ $fieldName }}" >
 </div>
